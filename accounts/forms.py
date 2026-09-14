2f758ddd7102daa55c092454b38c4f453dd37c48
Mamun Bepari <beparimamun708@gmail.com>
2026-09-14 08:47:07 +0600

    create profile update form

--- accounts/forms.py
+++ accounts/forms.py
@@ -22,4 +22,22 @@ class RegisterForm(UserCreationForm):
       for fieldname in self.fields.values():
          fieldname.widget.attrs["class"] = "form-control"
 
-         
+
+class ProfileUpdateForm(forms.ModelForm):
+   class Meta:
+      model = User
+      fields = (
+         "first_name",
+         "last_name",
+         "email",
+         "phone",
+         "date_of_birth",
+         "address",
+         "profile_picture",
+      )  
+
+   def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+
+      for field in self.fields.values():
+         field.widget.attrs["class"] = "form-control"
