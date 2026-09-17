a83948658897c9db68ee5708ea2bab73ceac8d3c
Mamun Bepari <beparimamun708@gmail.com>
2026-09-17 10:58:53 +0600

    Create Category view, Category Url, Category main urls

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from .models import Category
+from .forms import CategoryForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+   ListView,
+   CreateView,
+   UpdateView,
+   DeleteView,
+)
 
-# Create your views here.
+class CategoryListView(ListView):
+   model = Category
+   template_name = "categories/category_list.html"
+   context_object_name = "categories"
+
+
+class CategoryCreateView(LoginRequiredMixin, CreateView):
+   model = Category
+   form_class = CategoryForm
+   template_name = "categories/category_form.html"
+   success_url = reverse_lazy("category_list")
+
+
+class CategoryUpdateView(LoginRequiredMixin, UpdateView):
+   mdoel = Category
+   form_class = CategoryForm
+   template_name = "categories/category_form.html"
+   success_url = reverse_lazy("cateogry_list")
+
+
+class CategoryDeleteView(LoginRequiredMixin, DeleteView):
+   model = Category
+   template_name = "categories/category_confirm_delete.html"
+   success_url = reverse_lazy("category_list")
+
