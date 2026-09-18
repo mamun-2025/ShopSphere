ed72b3538242d5fad1d504105562fc8e25823bec
Mamun Bepari <beparimamun708@gmail.com>
2026-09-18 21:47:53 +0600

    Create Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from categories.models import Category
 
-# Create your models here.
+class Product(models.Model):
+   name = models.CharField(max_length=100)
+
+   category = models.ForeignKey(
+      Category,
+      on_delete=models.PROTECT,
+      related_name="products",
+   )
+
+   description = models.TextField(blank=True)
+
+   sku = models.CharField(
+      max_length=100,
+      unique=True,
+   )
+
+   price = models.DecimalField(
+      max_digits=10,
+      decimal_places=2,
+   )
+
+   stock = models.PositiveIntegerField(
+      default=0,
+   )
+
+   is_acitve = models.BooleanField(
+      default=True,
+   )
+
+   created_at = models.DateTimeField(
+      auto_now_add=True
+   )
+
+   updated_at = models.DateTimeField(
+      auto_now=True,
+   )
+
+   class Meta:
+      ordering = ["-created_at"]
+
+   def __str__(self):
+      return self.name 
