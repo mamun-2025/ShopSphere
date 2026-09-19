b3ec8a037f4713a28a8a916625ccc437b7d153ba
Mamun Bepari <beparimamun708@gmail.com>
2026-09-19 21:07:12 +0600

    Completed ProductListView

--- products/views.py
+++ products/views.py
@@ -11,7 +11,17 @@ class ProductListview(ListView):
          is_active = True
       )
 
-   
+"""
+1. What is ListView?
+= ListView is a Django generic class-based view used to display a list of objects from a model.
+2. What does get_queryset() do>
+= get_queryset() allows us to customize which objects are returned by a class-based view.
+3. Why use context_object_name?
+= It allows us to define a meaningful name for the objects passed to the template.
+4. What is a QuerySet?
+= A QuerySet is a collection of database queries representing objects retrieved from a Django model.
+
+"""
 
 
 
