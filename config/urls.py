45247ac298535521f58f22e606e87e7e8ea98b9f
Mamun Bepari <beparimamun708@gmail.com>
2026-09-26 10:39:56 +0600

    Create review app and url configuration

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('categories/', include('categories.urls')),
     path('products/', include('products.urls')),
+    path('reviews/', include('reviews.urls')),
 ]
 if settings.DEBUG:
    urlpatterns += static(
